773bc83d3f2aa382886191985ff159e38b01b10f
aheartforspinach <nsr97@gmx.de>
2019-12-14 15:52:57 +0100

    bugfix

--- inc/datahandlers/postingreminder.php
+++ inc/datahandlers/postingreminder.php
@@ -43,24 +43,25 @@ class postingreminderHandler {
         $date = new DateTime(date("Y-m-d", time())); //heute
         date_sub($date, date_interval_create_from_date_string($dayDifference.'days'));
 
-        $scenes = $db->simple_select('threads t join ' . TABLE_PREFIX . 'forums f on f.fid = t.fid', 't.lastpost, t.tid, t.subject', "find_in_set($inplayID, f.parentlist) and find_in_set($uid, t.partners)");
+        $scenes = $db->simple_select('threads t join ' . TABLE_PREFIX . 'forums f on f.fid = t.fid', 't.lastpost, t.tid, t.partners,t.lastposteruid', "find_in_set($inplayID, f.parentlist) and find_in_set($uid, t.partners)", array("order_by" => "t.lastpost","order_dir" => 'ASC'));
         while($scene = $db->fetch_array($scenes)){
             if($date->getTimestamp() > $scene['lastpost']){
-                $user = get_user($uid);
-                $allowed = true;
-                $groups = explode(',', $mybb->settings['postingreminder_groups']);
-
-                foreach($groups as $group){
-                    if($group == $user['usergroup']){
-                        $allowed = false;
+                if(getNextInRow(explode(',', $scene['partners']), $scene['lastposteruid']) == $uid){
+                    $user = get_user($uid);
+                    $allowed = true;
+                    $groups = explode(',', $mybb->settings['postingreminder_groups']);
+    
+                    foreach($groups as $group){
+                        if($group == $user['usergroup']){
+                            $allowed = false;
+                        }
                     }
-                }
-                if($allowed){
-                    array_push($returnArray, $scene['tid']);
+                    if($allowed){
+                        array_push($returnArray, $scene['tid']);
+                    }
                 }
             }
         }
-        asort($returnArray);
         return $returnArray;
     }
 
@@ -76,26 +77,33 @@ class postingreminderHandler {
         $date = new DateTime(date("Y-m-d", time())); //heute
         date_sub($date, date_interval_create_from_date_string($dayDifference.'days'));
 
-        $scenes = $db->simple_select('threads t join ' . TABLE_PREFIX . 'forums f on f.fid = t.fid', 't.tid, t.partners, t.subject, t.lastposteruid, t.lastpost', "find_in_set($inplayID, f.parentlist)", array("order_by" => "lastpost","order_dir" => 'ASC'));
+        $scenes = $db->simple_select('threads t join ' . TABLE_PREFIX . 'forums f on f.fid = t.fid', 't.tid, t.partners,t.lastposteruid, t.lastpost', "find_in_set($inplayID, f.parentlist)", array("order_by" => "lastpost","order_dir" => 'ASC'));
         while($scene = $db->fetch_array($scenes)){
-            if($date->getTimestamp() > $scene['lastpost']){
-                $nextInRow = '';
-                $participants = explode(',', $scene['partners']);
-                $ctr = 0;
-                foreach($participants as $participant){
-                    if($participant == $scene['lastposteruid']){
-                        if($participants[$ctr + 1] == null){
-                            $nextInRow = $participants[0];
-                        }else{
-                            $nextInRow = $participants[$ctr + 1];
-                        }
-                    }
-                    $ctr++;
-                }
-                $returnArray[$scene['tid']] = $nextInRow;
+            if($date->getTimestamp() > $scene['lastpost']){++;
+                $returnArray[$scene['tid']] = getNextInRow(explode(',', $scene['partners']), $scene['lastposteruid']);
             }
         }
 
         return $returnArray;
     }
 }
+
+    /**
+     *
+     * @return int with uid form user who must post next
+     */
+    function getNextInRow($participants, $lastPosterUid){
+        $ctr = 0;
+        $nextInRow = '';
+        foreach($participants as $participant){
+            if($participant == $lastPosterUid){
+                if($participants[$ctr + 1] == null){
+                    $nextInRow = $participants[0];
+                }else{
+                    $nextInRow = $participants[$ctr + 1];
+                }
+            }
+            $ctr++;
+        }
+        return $nextInRow;
+    }
